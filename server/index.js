923153da9ead2848989d2d7573764241a5fdef61
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-02-27 22:49:56 +0530

    Reformat server/index.js for improved readability

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ import notesRoutes from "./routes/notes.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000; //to allow dynamic port assignment (Explanation below:)
-
+const port = process.env.PORT || 3000; //to allow dynamic port assignment
 app.use(cors());
 app.use(express.json()); // IMPORTANT
 app.use("/api/notes", notesRoutes);
@@ -15,11 +14,3 @@ app.use("/api/notes", notesRoutes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-// This means:
-// If PORT exists in .env → use that
-// Otherwise → default to 3000
-// Since you don’t have PORT defined…
-// 👉 Your Express server runs on 3000
-// That is totally fine.
